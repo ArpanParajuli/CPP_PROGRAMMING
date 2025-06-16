fb48ca1341f89e7e71d6eebb4d575de9ae9905a4
ArpanParajuli <arpanparajuli394@gmail.com>
2025-06-16 08:38:21 +0545

    fourth

--- OOP/Polymorphism.cpp
+++ OOP/Polymorphism.cpp
@@ -3,10 +3,21 @@
 class Parent{
 
  public:
+
+ int a;
    virtual void collision(){
      std::cout << "This is Base collision";
    }
 
+   // function overloading (sum function with int and string work)
+   int sum(int a , int b){ return a + b;}
+   
+
+   std::string sum(std::string firstName , std::string lastName){ return firstName + " " + lastName;} // operator overloading
+
+      virtual ~Parent(){
+      std::cout << "This is Parent destructor!";
+    }
 };
 
 class Child : public Parent{
@@ -16,11 +27,30 @@ class Child : public Parent{
     void collision() override {
         std::cout << "This is derived collision";
     }
+
+
+    virtual ~Child(){
+      std::cout << "This is child destructor!"; // for proper clean up of object
+    }
 };
 
 int main(){
+   
 
-    Parent * p = new Child();
+
+  // std::cout << sizeof(Parent);
+    Parent * p = new Child(); // runtime polymorphism
      p->collision();
+
+
+    int result =  p->sum(10 , 20);
+
+    std::cout << "Sum int = " << result << std::endl;
+    std::cout << "Sum string = " << p->sum("Arpan","Parajuli");
+
+     delete p;
+
+
+     return 0;
 
 }
